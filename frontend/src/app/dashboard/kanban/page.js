13d85a83d4c0ab2bff8162e0e1fe339af05df96f
Pauline Ramelet <pauline.ramelet@icloud.com>
2026-09-25 16:08:41 +0200

    feat: setup routing, layout and folder structure

--- frontend/src/app/dashboard/kanban/page.js
+++ frontend/src/app/dashboard/kanban/page.js
@@ -1,3 +1,3 @@
 export default function Page() {
-  return <h1>Accueil</h1>;
+  return <h1>Kanban</h1>;
 }
--- frontend/src/app/projects/project/page.js
+++ frontend/src/app/projects/project/page.js
@@ -1,3 +1,3 @@
 export default function Page() {
-  return <h1>Accueil</h1>;
+  return <h1>Project</h1>;
 }
